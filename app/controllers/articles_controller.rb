ac002bb20e3fdcfdfb36b0c28458f162a36ead98
sohaib-a-k <sohaib@fliclicks.com>
2019-09-06 10:46:05 +0500

    CRUD

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+
+def new
+@article = Article.new
+end
+
+def index
+@articles = Article.all
+end
+
+def show
+@article = Article.find(params[:id])
+end
+
+def edit
+@article = Article.find(params[:id])
+end
+
+def destroy
+@article = Article.find(params[:id])
+ @article.destroy
+	flash[:notice] = "Article is successfully deleted"
+	redirect_to articles_path
+end
+
+
+def update
+	@article = Article.find(params[:id])
+if @article.update(article_params)
+	flash[:notice] = "Article has been updated successfully"
+	redirect_to article_path(@article)
+else
+	render 'edit'
+end
+end
+
+def create
+@article = Article.new(article_params);
+if @article.save
+	flash[:notice] = "Article has been created successfully"
+redirect_to article_path(@article)
+else 
+	render 'new'
+end
+end
+
+private def article_params
+params.require(:article).permit(:title,:description)
+end
+end
